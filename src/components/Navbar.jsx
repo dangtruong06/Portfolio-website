7a9cf7f3a9514ac5eed98264656404509916c2ab
dangtruong06 <DANGTT066@GMAIL.COM>
2026-08-08 09:17:24 -0700

    update resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ function Navbar() {
         <nav className="navbar">
             <div className="nav-logo">Tony Dang.</div>
             <div className="nav-links">
-                <a href="#projects">projects</a>
                 <a href="#experience">experience</a>
+                <a href="#projects">projects</a>
                 <a href="#contact">contact</a>
             </div>
         </nav>
